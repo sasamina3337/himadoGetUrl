331ec8d512358babaa0fa7e37245fd14845dfa98
sasamina <38094498+sasamina3337@users.noreply.github.com>
2024-07-07 00:49:00 +0900

    Update HimadoGetterl.user.js
    バージョン更新

--- HimadoGetterl.user.js
+++ HimadoGetterl.user.js
@@ -2,7 +2,7 @@
 // @name         ひまわり動画ゲッター
 // @namespace    https://github.com/sasamina3337/
 // @description  ひまわり動画内に動画url取得用のボタンを表示させる
-// @version      1.14
+// @version      1.20
 // @author       sasamina
 // @match        http://himado.in/*
 // @match        https://web.archive.org/*
